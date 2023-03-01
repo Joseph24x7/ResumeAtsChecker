08ead4b395be966a6034f5e610aff5839482720d
joseph24x7 <joseph2471994@gmail.com>
2023-03-01 22:48:01 +0530

    nlp a start

--- src/main/java/com/text/to/video/controller/TextToVideoController.java
+++ src/main/java/com/text/to/video/controller/TextToVideoController.java
@@ -1,15 +1,25 @@
 package com.text.to.video.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.text.to.video.service.NlpService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class TextToVideoController {
 
-    @PostMapping("/")
-    public String helloWorld(@RequestBody String data) {
-        return "Hellow World";
-    }
+	private final NlpService nlpService;
+
+	@PostMapping("/")
+	public List<String> helloWorld(@RequestBody String data) {
+		List<String> keywords = nlpService.getKeywords(data);
+		return keywords;
+	}
 
 }
