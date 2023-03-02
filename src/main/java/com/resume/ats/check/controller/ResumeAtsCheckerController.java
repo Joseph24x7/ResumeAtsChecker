e1dabac9f566346aaadd950aeb3b69f4735c1924
joseph24x7 <joseph2471994@gmail.com>
2023-03-02 22:35:45 +0530

    pdf scanner

--- src/main/java/com/resume/ats/check/controller/ResumeAtsCheckerController.java
+++ src/main/java/com/resume/ats/check/controller/ResumeAtsCheckerController.java
@@ -1,15 +1,24 @@
 package com.resume.ats.check.controller;
 
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ResumeAtsCheckerController {
 
-	@PostMapping("/")
-	public String helloWorld(@RequestBody String data) {
-		return data;
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+	    PDDocument document = PDDocument.load(file.getInputStream());
+	    PDFTextStripper stripper = new PDFTextStripper();
+	    String content = stripper.getText(document);
+	    document.close();
+	    return content;
 	}
 
 }
